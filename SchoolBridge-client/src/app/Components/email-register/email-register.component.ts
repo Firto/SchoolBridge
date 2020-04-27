30c8ada8a8c1013e478c9f17cba21eafa1eef4ea
bodya <bbojcuk44>
2020-04-27 16:17:29 +0300

    merge from master

---
+++ SchoolBridge-client/src/app/Components/email-register/email-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-register',
+  templateUrl: './email-register.component.html',
+  styleUrls: ['./email-register.component.css']
+})
+export class EmailRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- SchoolBridge-client/src/app/app.module.ts
+++ SchoolBridge-client/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { EmailRegisterComponent } from './Components/email-register/email-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    EmailRegisterComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
